a64f69c1b2646d46dd8a5f09ca1ab35a2d23576d
Dennis <dmarlo96@live.ca>
2021-04-25 19:59:36 -0400

    User Model

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = express();
 // Connect to database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
