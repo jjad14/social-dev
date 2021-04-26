9c0a7cf039a5dc2794a7b5584ffe3e45b4538f10
Dennis <dmarlo96@live.ca>
2021-04-25 21:18:47 -0400

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,69 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
+
 const router = express.Router();
 
-
-// @route   GET api/users
-// @desc    Test Route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+    '/',
+    [
+        body('name', 'A Name is required').not().isEmpty(), 
+        body('email', 'A valid email is required').isEmail(),
+        body('password', 'A valid password with a min of 6 characters is required').isLength({min: 6})
+    ],
+    async (req, res) => {
+        // check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password} = req.body;
+
+        try {
+            // check if the email is in use
+            let user = await User.findOne({ email });
+
+            if (user) {
+                res.status(400).json({ errors: [ {msg: 'User already exists'} ] });
+            }
+
+            // Get Users Gravitar
+            const avatar = gravatar.url(email, {
+                s: '200', // size
+                r: 'pg', // rating: censorship 
+                d: 'mm' // default: mm is for mystery man
+            });
+
+            // user instance
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            // Hash Password
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            // save user to db
+            await user.save();
+
+            // Return token   
+            res.send('User registered');
+
+        } catch (error) {
+            res.status(500).send('Server Error');
+        }
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect to database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
