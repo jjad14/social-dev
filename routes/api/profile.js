bc1cf33d081367ddb94dd5af236f62c3ddd2399a
Dennis <dmarlo96@live.ca>
2021-04-26 22:16:40 -0400

    Get Github Repositories

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -273,10 +273,95 @@ router.delete('/experience/:expId', auth, async (req, res) => {
   
       await profile.save();
 
-      res.status(200).json(foundProfile);
+      res.status(200).json(profile);
     } catch (error) {
         res.status(500).json({ msg: 'Server error' });
     }
-  });
+});
+
+// @route  PUT api/profile/education
+// @desc   Add education to profile
+// @access Private
+router.put(
+    '/education', 
+    [
+        auth, 
+        [
+            body('school', 'A School is required').not().isEmpty(),
+            body('degree', 'A Degree is required').not().isEmpty(),
+            body('fieldofstudy', 'A Field of Study is required').not().isEmpty(),
+            body('from', 'A From Date is required').not().isEmpty()
+        ]
+    ], 
+    async (req, res) => {
+        
+    // check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // pull fields from request body
+    const {school, degree, fieldofstudy, from, to, current, description} = req.body;
+
+    // create a new education object
+    const newEducation = {school, degree, fieldofstudy, from, to, current, description};
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // unshift pushes to begining rather than end (push)
+        profile.education.unshift(newEducation);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        res.status(500).send('Server Error');        
+    }
+});
+
+// @route  DELETE api/profile/education/:eduId
+// @desc   Delete Profile Education
+// @access Private
+router.delete('/education/:eduId', auth, async (req, res) => {
+    try {
+      // find profile by user id
+      const profile = await Profile.findOne({ user: req.user.id });
+  
+      // filter out the experience using query parameter
+      profile.education = profile.education.filter(
+        exp => exp._id.toString() !== req.params.eduId
+      );
+  
+      await profile.save();
+
+      res.status(200).json(profile);
+    } catch (error) {
+        res.status(500).json({ msg: 'Server error' });
+    }
+});
+
+// @route  GET api/profile/github/:username
+// @desc   Get repositories from github
+// @access Public
+router.get('/github/:username', async (req, res) => {
+    try {
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+  
+      res.json(gitHubResponse.data);
+    } catch (err) {
+      res.status(404).json({ msg: 'No Github profile found' });
+    }
+});
 
 module.exports = router;
