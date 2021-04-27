ffec0fb37f7aed048d87f2ea77cfe0e36de62422
Dennis <dmarlo96@live.ca>
2021-04-27 17:33:06 -0400

    Add and Remove Comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -72,7 +72,7 @@ router.get('/:id', auth, async (req, res) => {
     
     try {
         // check if id is a valid ObjectId
-        if (!mongoose.Types.ObjectId.isValid(post_id)) {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
             return res.status(404).json({ msg: 'Post was not found' });
         }
 
@@ -97,7 +97,7 @@ router.delete('/:id', auth, async (req, res) => {
     
     try {
         // check if id is a valid ObjectId
-        if (!mongoose.Types.ObjectId.isValid(post_id)) {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
             return res.status(404).json({ msg: 'Post was not found' });
         }
 
@@ -128,7 +128,7 @@ router.delete('/:id', auth, async (req, res) => {
 router.put('/like/:id', auth, async (req, res) => {
     try {
         // check if id is a valid ObjectId
-        if (!mongoose.Types.ObjectId.isValid(post_id)) {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
             return res.status(404).json({ msg: 'Post was not found' });
         }
 
@@ -162,7 +162,7 @@ router.put('/like/:id', auth, async (req, res) => {
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
         // check if id is a valid ObjectId
-        if (!mongoose.Types.ObjectId.isValid(post_id)) {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
             return res.status(404).json({ msg: 'Post was not found' });
         }
 
@@ -191,6 +191,108 @@ router.put('/unlike/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route  POST api/posts/comment/:id
+// @desc   Add a comment to post
+// @access Private
+router.post(
+    '/comment/:id', 
+    [
+        auth,
+        [
+            body('text', 'Text is required').not().isEmpty()
+        ]
+    ], 
+    async (req, res) => {
+    
+    // check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        // check if id is a valid ObjectId
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(404).json({ msg: 'Post was not found' });
+        }
+
+        // get user
+        const user = await User
+            .findById(req.user.id)
+            .select('-password');
+
+        // get post
+        const post = await Post.findById(req.params.id);
+
+        // create comment object
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        // add comment to post
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        // return all comments
+        res.json(post.comments);
+    } catch (error) {
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    DELETE api/posts/comment/:id/:commentId
+// @desc     Delete comment from post
+// @access   Private
+router.delete(
+    '/comment/:id/:commentId', 
+    auth, 
+    async (req, res) => {
+
+    try {
+
+      // check if ids are valid object Ids
+      if (!mongoose.Types.ObjectId.isValid(req.params.id) || 
+          !mongoose.Types.ObjectId.isValid(req.params.commentId)) 
+      {
+        return res.status(404).json({ msg: 'Post was not found' });
+      }
+
+      // Get Post by Id
+      const post = await Post.findById(req.params.id);
+  
+      // Get comment from Post by commentId
+      const comment = post.comments.find(comment => 
+        comment.id === req.params.commentId
+      );
+
+      // Check if comment exists
+      if (!comment) {
+        return res.status(404).json({ msg: 'The Comment does not exist' });
+      }
+
+      // Check if current user has ownership of the comment
+      if (comment.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'User is not authorized' });
+      }
+  
+      // filter out comment in post
+      post.comments = post.comments.filter(({id}) => 
+        id !== req.params.commentId
+      );
+  
+      await post.save();
+  
+      // return posts
+      res.json(post.comments);
+    } catch (err) {
+       res.status(500).send('Server Error');
+    }
+  });
 
 
 module.exports = router;
