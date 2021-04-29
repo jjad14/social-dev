9a7ee9d89890465d5916eafda4e8817d35b9886c
Dennis <dmarlo96@live.ca>
2021-04-28 21:12:17 -0400

    Create Redux Store - Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -9,20 +13,22 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
 
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </Switch>
-      </section>
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </section>
 
-    </Fragment>
-  </BrowserRouter>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+// export all reducers
+export default combineReducers({
+
+});
