2137ffdc029459675e334a1d7b775f84c1c6a3d7
Dennis <dmarlo96@live.ca>
2021-04-28 20:10:49 -0400

    Register Form

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
+// import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,91 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChangeHandler = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value});
+    };
+    
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        // passwords match
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        }
+        else {
+            console.log(formData);
+        }
+
+    };
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <section className="container card">
+                <h1 className="large text-primary text-center">Sign Up</h1>
+                <p className="lead text-center"><i className="fas fa-user"></i> Create Your Account</p>
+                <form className="form" onSubmit={onSubmitHandler}>
+                    <div className="form-group">
+                        <input 
+                            type="text" 
+                            placeholder="Name" 
+                            name="name" 
+                            required
+                            value={name}
+                            onChange={e => onChangeHandler(e)}/>
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="email" 
+                            placeholder="Email Address" 
+                            name="email"
+                            value={email}
+                            required
+                            onChange={e => onChangeHandler(e)}/>
+
+                        <small className="form-text">
+                            To show a profile image, it is recommended to use a Gravatar email
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            minLength="6"
+                            value={password}
+                            required
+                            onChange={e => onChangeHandler(e)}/>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            name="password2"
+                            value={password2}
+                            minLength="6"
+                            required
+                            onChange={e => onChangeHandler(e)}/>
+                    </div>
+                    <div className="text-center">
+                        <input type="submit" 
+                            className="btn btn-primary" 
+                            value="Register" />
+                    </div>
+                </form>
+                    <p className="my-1 text-center">
+                        Already have an account? <a href="login.html">Sign In</a>
+                    </p>
+            </section>
+        </Fragment>
     );
 };
 
