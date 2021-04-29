e3760cf0092a6c8d5c5d410fefb01a95f91c4f97
Dennis <dmarlo96@live.ca>
2021-04-28 22:36:50 -0400

    Remove Alerts via Timeout

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,19 @@
 import { v4 as uuidv4 } from 'uuid';
 import * as types from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, time = 3000) => dispatch => {
     const id = uuidv4();
 
     dispatch({
         type: types.SET_ALERT,
         payload: { msg, alertType, id}
     });
+
+    setTimeout(() => {
+        dispatch({ type: types.REMOVE_ALERT, payload: id});
+    }, time);
+};
+
+const removeAlert = () => {
 
 };
